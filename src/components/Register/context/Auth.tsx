8ef529d87d61d6482bf7dbe8eaf0a318c2283341
Mateusz <mateusz4k@outlook.com>
2023-07-08 08:25:36 +0200

    removed folder duplicates

--- src/components/Register/context/Auth.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { User } from '@/config/firebase'
-const initValues = {
-	listenOnSubmitForm: (value?: boolean, username?: string) => {},
-	logout: () => {},
-	authUser: {} as User | null
-}
-
-export const AuthCtx = React.createContext(initValues)
