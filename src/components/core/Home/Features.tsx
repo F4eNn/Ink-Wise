c4944ddbb06690bdf6594401b67b194e771b3ba1
Mateusz <mateusz4k@outlook.com>
2023-07-20 21:32:10 +0200

    Revert "Merge branch 'main' of https://github.com/F4eNn/Ink-Wise"
    This reverts commit ec66a36d7afe0b7b23d81c95dffc941a9363abd2, reversing
    changes made to eb3d19a6d8896d1fd4c791c5da203040191b1eca. revert bad pull

---
+++ src/components/core/Home/Features.tsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import { AspectRatio, Box, Flex } from '@/lib/chakra'
+import Image from 'next/image'
+import { Feature } from './ui/Feature'
+import { BlobFeatures } from './ui/BlobFeatures'
+export const Features = () => {
+	return (
+		<Box mt={['36', null, '56']}>
+			<Box
+				as='h2'
+				textAlign='center'
+				textStyle='h2'>
+				Innovative Capabilities
+			</Box>
+			<Box
+				pos='relative'
+				mt={{ base: '100px', xl: '400px' }}
+				display='flex'
+				flexDir='column'
+				alignItems='center'
+				gap='10'
+				justifyContent='center'>
+				<AspectRatio
+					order='2'
+					width={['100%', '85%', null, '60%', '50%']}
+					ratio={1}>
+					<Image
+						src='/Features-1.png'
+						alt=''
+						fill
+					/>
+				</AspectRatio>
+				<Feature
+					top='-100px'
+					left='0'
+					title='Intuitive Organization'
+					desc='Easily organize your notes with our intuitive system of categories, tags, and search functionality. Find the information you need at a glance.'
+				/>
+				<Flex
+					pos='relative'
+					gap='5'
+					w='full'
+					zIndex='0'
+					mb='10'
+					flexDir={{ base: 'column', md: 'row' }}
+					alignItems={{ base: 'center', md: 'unset' }}
+					justifyContent='space-around'>
+					<Feature
+						top='-300px'
+						left='50%'
+						translate='-50%'
+						title='Sleek and Minimalist Design'
+						desc='Enjoy a sleek and minimalist design that provides a clutter-free environmentt, allowing you to focus on your notes and boost your productivity.'
+					/>
+					<Feature
+						top='-100px'
+						right='0'
+						title='Notes in the New Era'
+						desc='Prepare for a new era of note-taking. Our Modern Notepad app combines advanced features with user-friendly design, providing you with the ultimate tools for creating and organizing notes.'
+					/>
+					<BlobFeatures />
+				</Flex>
+				<Flex
+					w='full'
+					gap='5'
+					flexDir={{ base: 'column', md: 'row' }}
+					alignItems={{ base: 'center', md: 'unset' }}
+					justifyContent='space-around'
+					order='3'>
+					<Feature
+						bottom='-100px'
+						right='0'
+						title='Seamless Multimedia Integration'
+						desc=' Seamlessly integrate multimedia elements like images, audio clips, and videos into your notes. Bring your ideas to life with dynamic and engaging content.'
+					/>
+					<Feature
+						bottom='-100px'
+						left='0'
+						title='Cross-Device Accessibility'
+						desc='Access your notes anytime, anywhere, across multiple devices. Our modern notepad app works seamlessly on computers, smartphones, and tablets, ensuring full synchronization.'
+					/>
+				</Flex>
+			</Box>
+		</Box>
+	)
+}
---
+++ src/components/core/user/Forms/UpdateUserData.tsx
@@ -0,0 +1,98 @@
+import { Box, Button, CardHeader, Flex, FormControl, FormLabel, Input, Textarea } from '@/lib/chakra'
+import React, { useEffect } from 'react'
+import { Avatar } from '../ui/Avatar'
+import { useForm } from 'react-hook-form'
+
+import { useAuth } from '@/hooks/useAuth'
+
+import { setProfileUpdate } from '../../../../helpers/editProfile'
+import { updateProfile } from '../../../../helpers/editProfile'
+
+import { UsernameInput } from '@/components/ui/UsernameInput'
+import { FormData } from '../../../../helpers/editProfile'
+interface FormProps {
+	onToggle: () => void
+	userId: string | undefined
+	valueBio: string
+	valueName: string
+	valuePhoto: string
+}
+
+export const UpdateUserData = ({ valueBio, userId, valueName, valuePhoto, onToggle }: FormProps) => {
+	const { register, handleSubmit, setValue, formState } = useForm<FormData>({
+		defaultValues: {
+			bio: '',
+			username: '',
+			photo: '',
+		},
+	})
+	const { errors } = formState
+	const { authUser } = useAuth()
+
+	const onSubmit = async (data: FormData) => {
+		await setProfileUpdate(data, userId)
+		await updateProfile({ username: data.username, photo: data.photo }, authUser)
+		onToggle()
+	}
+	useEffect(() => {
+		setValue('photo', valuePhoto)
+		setValue('bio', valueBio)
+		setValue('username', valueName)
+	}, [setValue, valueBio, valueName, valuePhoto])
+
+	return (
+		<Box
+			as='form'
+			w='full'
+			onSubmit={handleSubmit(onSubmit)}>
+			<CardHeader>
+				<Flex
+					gap='7'
+					alignItems='center'>
+					<Box>
+						<Avatar size='lg' />
+					</Box>
+					<Flex
+						w='full'
+						fontSize='lg'
+						flexDir='column'
+						gap='3'>
+						<UsernameInput
+							errors={errors.username?.message}
+							register={register}
+						/>
+					</Flex>
+				</Flex>
+			</CardHeader>
+			<FormControl>
+				<FormLabel htmlFor='profilePicture'>Profile Picture</FormLabel>
+				<Input
+					id='profilePicture'
+					borderColor='borderColor'
+					focusBorderColor='primary.900'
+					placeholder='Photo URL'
+					{...register('photo')}
+				/>
+			</FormControl>
+			<FormControl mt='5'>
+				<FormLabel htmlFor='description'>About You</FormLabel>
+				<Textarea
+					borderColor='borderColor'
+					id='description'
+					placeholder='Add a bio'
+					resize='none'
+					h='125px'
+					focusBorderColor='primary.900'
+					{...register('bio')}
+				/>
+			</FormControl>
+			<Button
+				variant='primary'
+				w='full'
+				type='submit'
+				mt='10'>
+				Save
+			</Button>
+		</Box>
+	)
+}
